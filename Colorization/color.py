b8f6d0770c4ce14dd2e82b16c5cb192654a1ff89
Alexander Bogdanov <a@bogdanov.co>
2022-01-19 15:33:51 +0300

    Update config

--- Colorization/color.py
+++ Colorization/color.py
@@ -2,7 +2,6 @@ import rtoml
 from pathlib import Path
 from loguru import logger
 from color.config import Config
-from notifiers.logging import NotificationHandler
 from color.train import train as _train
 import click
 
@@ -18,33 +17,14 @@ config: Config = None
               default=Path('config.toml'))
 def cli(config_file):
     logger.info(f'Using {config_file} config')
-    global config, notifier
+    global config
     config = Config.parse_obj(rtoml.load(config_file))
-    params = {
-        "token": config.token,
-        "chat_id": config.chat_id
-    }
-    handler = NotificationHandler("telegram", defaults=params)
-    logger.add(handler, level="ERROR")
-    logger.add(handler, level="SUCCESS")
-
-
-@cli.command()
-def prepare():
-    _prepare(config.data_path)
-    logger.success("Preparation complete")
 
 
 @cli.command()
 def train():
     _train(config)
     logger.success("Training complete")
-
-
-@cli.command()
-def test():
-    _test(config)
-    logger.success("Testing complete")
 
 
 if __name__ == '__main__':

ba5f8b1ae3bb9071b1830b1d8d33dc2d8aed96c3
Alexander Bogdanov <a@bogdanov.co>
2022-01-20 12:45:55 +0300

    Add colorization command and wandb functionality

--- Colorization/color.py
+++ Colorization/color.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from loguru import logger
 from color.config import Config
 from color.train import train as _train
+from color.colorize import colorize as _colorize
 import click
 
 config: Config = None
@@ -11,9 +12,7 @@ config: Config = None
 @click.group()
 @click.option('-c',
               '--config_file',
-              type=click.Path(exists=True,
-                              dir_okay=False,
-                              readable=True),
+              type=click.Path(exists=True, dir_okay=False, readable=True),
               default=Path('config.toml'))
 def cli(config_file):
     logger.info(f'Using {config_file} config')
@@ -25,6 +24,13 @@ def cli(config_file):
 def train():
     _train(config)
     logger.success("Training complete")
+
+
+@cli.command()
+@click.argument('input_img', type=click.Path(exists=True, dir_okay=False, readable=True))
+@click.argument('output_img', type=click.Path(dir_okay=False, writable=True))
+def colorize(input_img, output_img):
+    _colorize(input_img, output_img)
 
 
 if __name__ == '__main__':
--- Colorization/color/config.py
+++ Colorization/color/config.py
@@ -3,12 +3,18 @@ from pydantic import BaseModel, validator
 
 
 class Config(BaseModel):
+    # model config
     batch_size: int
     point_batches: int
     lr: float
     lr_decay: float
     data_path: Path
     total_epoch: int
+
+    # wb config
+    wb_enabled: bool = False
+    project_name: str
+    entity: str
 
     @validator('data_path')
     def data_path_must_be_directory(cls, v: Path):
